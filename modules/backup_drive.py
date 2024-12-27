a18354ef378ec2b333c07ee74f397c97d6872b51
Eli Vermeulen <eli.vermeulen2002@gmail.com>
2024-12-27 14:00:51 +0100

    Add backup script with dropbox

--- modules/backup_drive.py
+++ modules/backup_drive.py
@@ -1,14 +1,16 @@
 import dropbox
-import os
+import paramiko
+from io import BytesIO
+from datetime import datetime
 
 def read_access_token():
-    token_file_path = 'token.txt'  
+    token_file_path = 'token.txt'
     try:
         with open(token_file_path, 'r') as f:
             access_token = f.read().strip()  
             return access_token
     except FileNotFoundError:
-        print(f"File doenst exist")
+        print(f"File doesn't exist")
         return None
 
 
@@ -18,19 +20,55 @@ def initialize_dropbox_client():
         raise Exception("Token error")
     return dropbox.Dropbox(access_token)
 
-def upload_to_dropbox(local_file_path, dropbox_dest_path):
-    dbx = initialize_dropbox_client()
-    with open(local_file_path, 'rb') as f:
-        dbx.files_upload(f.read(), dropbox_dest_path, mute=True)
-    print(f'Uploaded {local_file_path} to Dropbox as {dropbox_dest_path}')
 
-def backup_directory(directory_path):
-    for filename in os.listdir(directory_path):
-        local_file_path = os.path.join(directory_path, filename)
-        if os.path.isfile(local_file_path):
-            dropbox_dest_path = f'/backup/{filename}'  # Change the folder name as needed
-            upload_to_dropbox(local_file_path, dropbox_dest_path)
+def upload_to_dropbox(file_content, dropbox_dest_path):
+    dbx = initialize_dropbox_client()
+    dbx.files_upload(file_content, dropbox_dest_path, mute=True)
+    print(f'File uploaded to dropbox: {dropbox_dest_path}')
+
+
+def execute_ssh_command(ssh_client, command):
+    stdin, stdout, stderr = ssh_client.exec_command(command)
+    return stdout.read().decode().strip(), stderr.read().decode().strip()
+
+
+def download_files_from_remote(remote_ip, remote_directory, ssh_username):
+    try:
+        ssh_client = paramiko.SSHClient()
+        ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        ssh_client.load_system_host_keys()
+        ssh_client.connect(hostname=remote_ip, username=ssh_username)
+
+        
+        sftp = ssh_client.open_sftp()
+
+        
+        remote_files = sftp.listdir(remote_directory)
+        print(f"files on host: {remote_files}")  
+
+        
+        for filename in remote_files:
+            remote_file_path = remote_directory + '/' + filename  
+            print(f"accesing: {remote_file_path}")  
+            
+            
+            with sftp.open(remote_file_path, 'rb') as remote_file:
+                file_content = remote_file.read()  
+
+                
+                dropbox_dest_path = f'/{remote_ip}/{filename}' 
+                upload_to_dropbox(file_content, dropbox_dest_path)
+
+        
+        sftp.close()
+        ssh_client.close()
+    except Exception as e:
+        print(e)
+
 
 if __name__ == '__main__':
-    directory_to_backup = r'C:\test\powershell' 
-    backup_directory(directory_to_backup)
+    remote_ip = '192.168.0.195'  
+    remote_directory = '/home/ubuntu/test'  
+    ssh_username = 'ubuntu'  
+
+    download_files_from_remote(remote_ip, remote_directory, ssh_username)
